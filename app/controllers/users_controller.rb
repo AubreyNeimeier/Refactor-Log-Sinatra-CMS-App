281110700654f14350be6c2d1afa39f0ef77784f
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-15 17:14:40 +0000

    built out most of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,39 @@
 class UsersController < ApplicationController
+  get "/signup" do
+    erb :"users/signup"
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "/users/:#{@user.slug}"
+    end
+  end
+
+  get "/login" do
+    erb :"/users/login"
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect to "/users/#{@user.slug}"
+     else
+       redirect "/login"
+     end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    @projects = Project.where(user_id: @user.id)
+    @cocnerns = Concern.where(user_id: @user.id)
+    #binding.pry
+    erb :"/users/index"
+  end
 
 end
